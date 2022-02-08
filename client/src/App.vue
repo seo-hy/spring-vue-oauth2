04d856b6c22754b1002baef79b0921601df46962
seoh02h <seok02h@khu.ac.kr>
2022-02-08 23:56:15 +0900

    라우터 전체 추가, navBar 수정

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <NavBar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
 export default {
   name: "app",
-  components: {
-    NavBar,
-  },
 };
 </script>
 
-<style></style>
+<style>
+body {
+  margin: 0px;
+}
+</style>
---
+++ client/src/components/nav/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nav-bar">
+    <div class="nav-bar-default">
+      <router-link to="/">HOME</router-link>
+      <router-link to="/info">INFO</router-link>
+      <router-link to="/community">COMMUNITY</router-link>
+      <router-link to="/log">LOG</router-link>
+    </div>
+
+    <div v-if="isAuth" class="nav-bar-auth">
+      <router-link to="/mypage">mypage</router-link>
+      <div>logout</div>
+    </div>
+    <div v-else class="nav-bar-auth">
+      <router-link to="/signin">signin</router-link>
+      <router-link to="/login">login</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isAuth: false,
+    };
+  },
+  methods: {
+    login() {},
+  },
+};
+</script>
+
+<style scoped>
+.nav_bar {
+  background-color: rgb(207, 206, 201);
+}
+</style>
