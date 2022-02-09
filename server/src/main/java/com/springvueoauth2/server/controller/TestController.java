2437bf782a7cfaada35ec61773b5d9e4c71fcfce
seoh02h <seok02h@khu.ac.kr>
2022-02-09 10:01:03 +0900

    spring 프로젝트 생성, 패키지 추가

--- server/src/main/java/com/springvueoauth2/server/controller/TestController.java
+++ server/src/main/java/com/springvueoauth2/server/controller/TestController.java
@@ -1,13 +1,24 @@
 package com.springvueoauth2.server.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/api/users", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api")
 public class TestController {
+
+  @SneakyThrows
+  @ApiOperation("커넥션 테스트")
+  @GetMapping("/test")
+  public String test(){
+    return "test";
+  }
 
 }
